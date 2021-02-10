41e8687a001cebf16dfbe6e321f2719a19ad1ae6
MatheusF99 <mc79846@gmail.com>
2021-02-10 15:07:37 -0300

    user created and abstracted

---
+++ src/controllers/CreateUsers.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import Users from '../models/Users'
+import { getRepository } from 'typeorm'
+
+export default {
+    async create(req: Request, res:Response){
+        const {
+            name,
+            phone,
+            email
+        } = req.body
+
+        const userRepository = getRepository(Users)
+
+        const user = userRepository.create({
+            name,
+            phone,
+            email
+        })
+
+        await userRepository.save(user)
+
+        return(
+            res.status(201).json(user)
+        )  
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,32 +1,9 @@
 import {Router} from 'express'
-import Users from './models/Users'
-import { getRepository } from 'typeorm'
+import CreateUser from './controllers/CreateUsers'
 
 
 const routes = Router()
 
-routes.post('/users', async (req, res)=>{
-
-    const {
-        name,
-        phone,
-        email
-    } = req.body
-
-    const userRepository = getRepository(Users)
-
-    const user = userRepository.create({
-        name,
-        phone,
-        email
-    })
-
-    await userRepository.save(user)
-
-    console.log(req.query)
-    return(
-        res.json({message:'hello world'})
-    )   
-})
+routes.post('/users', CreateUser.create)
 
 export default routes

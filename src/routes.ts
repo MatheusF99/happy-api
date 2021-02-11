876888ccb5313747cf847719153255ad797c719c
MatheusF99 <mc79846@gmail.com>
2021-02-11 20:58:55 -0300

    configurando o upload de imagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import {Router} from 'express'
+import multer from 'multer'
+import uploadConfig from './config/upload'
+
 import CreateUser from './controllers/CreateUsers'
 import ListUsers from './controllers/ListUsers'
 import ShowUsers from './controllers/ShowUsers'
-import Users from './models/Users'
 
+const upload = multer(uploadConfig)
 
 const routes = Router()
 
 routes.post('/users', CreateUser.create)
 routes.get('/users', ListUsers.index)
-routes.get('/users/:id', ShowUsers.show)
+routes.get('/users/:id', upload.array('images'),ShowUsers.show)
 
 export default routes

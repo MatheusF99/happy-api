0627c9a51c0ff9d26555373b3e2165bd9841533a
MatheusF99 <mc79846@gmail.com>
2021-02-13 08:40:53 -0300

    upload image configurate

---
+++ src/database/migrations/1613216212812-create_images.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1613216212812 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'images',
+        columns:[
+            {
+                name: 'id',
+                type:'integer',
+                unsigned: true, //essa coluna nao pode ser negativa
+                isPrimary: true, //essa coluna é uma PK
+                isGenerated: true, //essa coluna vai ser gerada automaticamente
+                generationStrategy: 'increment'
+            },
+            {
+                name: 'path',
+                type: 'varchar'
+            },
+            //aqui vai receber o id do usuario
+            {
+                name: 'user_id',
+                type: 'integer',
+            }
+        ],
+        foreignKeys:[
+            {
+                name: 'imageUser',
+                columnNames: ['user_id'],
+                referencedTableName: 'users',
+                referencedColumnNames: ['id'],
+                onUpdate:'CASCADE',
+                onDelete:'CASCADE'
+            }
+        ]
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('images')
+    }
+
+}

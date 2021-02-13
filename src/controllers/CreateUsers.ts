2c421550cfd7b849236a6deb50c24317d56473e2
MatheusF99 <mc79846@gmail.com>
2021-02-13 08:36:03 -0300

    configurando upload image

--- src/controllers/CreateUsers.ts
+++ src/controllers/CreateUsers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
-import Users from '../models/Users'
 import { getRepository } from 'typeorm'
+import Users from '../models/Users';
 
 export default {
 	async create(req: Request, res:Response){
